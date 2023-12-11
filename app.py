a6095ecdca1ae61bafc9270187a2e906b8234208
fabien5525 <fcolard9@gmail.com>
2023-12-11 15:31:04 +0100

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from function import scraping_bdm
 
 st.set_page_config(
     page_title="Streamlit App",
@@ -7,4 +8,32 @@ st.set_page_config(
     layout="wide",
 )
 
+# Champs de saisie de text pour les mots clef avec un bouton de validation
+# scrapper en fonction de la recherche les artciles surl e site BDM
+# Permettre à l'applidation de téélcharger les données en csv
 
+search = st.text_input('Recherche')
+
+if st.button('Valider'):
+    st.write('Vous avez choisi', search)
+
+    results_dict = {}
+
+    for index_page in range(1, 2): 
+        url = f'https://www.blogdumoderateur.com/page/{str(index_page)}/'
+
+        search_clean = search.replace(' ', '+')
+        url_search = url + '?s=' + search_clean
+
+        dict_page = scraping_bdm(url_search)
+        results_dict.update(dict_page)
+
+    df = pd.DataFrame(results_dict).T
+    st.write(df)
+
+    st.download_button(
+        label="Télécharger les données",
+        data=df.to_csv().encode('utf-8'),
+        file_name='bdm.csv',
+        mime='text/csv'
+    )
--- function.py
+++ function.py
@@ -2,11 +2,10 @@ import json
 from bs4 import BeautifulSoup
 import requests
 
-def scraping_bdm():
 
-    url = 'https://www.blogdumoderateur.com/'
+def scraping_bdm(url):
     response_bdm = requests.get(url)
-    soup_bdb = BeautifulSoup(response_bdm.text)
+    soup_bdb = BeautifulSoup(response_bdm.content, 'html.parser')
 
     article_dict = {}
     articles = soup_bdb.find_all('article')
@@ -15,19 +14,24 @@ def scraping_bdm():
         id = article.get('id')
 
         title = article.find('h3').text.replace('\xa0', ' ')            # Title
-        
-        try:image = article.find('img')['data-lazy-src']                # Image
-        except:image = None
-       
-        try:link = article.find('a')["href"]                            # Link
-        except:link = article.parent['href']
 
-        try:theme = article.find('span', 'favtag').text                 # CatÃ©gorie
-        except:theme = article.find_previous('h2').text
-        
+        try:
+            image = article.find('img')['src']                # Image
+        except:
+            image = None
+
+        try:
+            link = article.find('a')["href"]                            # Link
+        except:
+            link = article.parent['href']
+
+        theme = article.find('span', 'favtag').text
+
         date = article.find('time')['datetime'].split('T')[0]           # Date
-        
-        article_dict[id] = { 'title' :title, 'date'  :date, 'link':link, 'image' :image, 'categorie' : theme }
 
-    with open ('bdm.json', 'w') as f:json.dump(article_dict, f)
+        article_dict[id] = {'title': title, 'date': date,
+                            'link': link, 'image': image, 'categorie': theme}
+
+    with open('bdm.json', 'w') as f:
+        json.dump(article_dict, f)
     return article_dict
